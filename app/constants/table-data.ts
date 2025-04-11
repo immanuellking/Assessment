c17e10deba11c314cfa2f1852c14638e179d8324
immanuellking <emmaebubeegbosi@gmail.com>
2025-04-11 13:06:53 +0100

    moved data to constants file

---
+++ app/constants/table-data.ts
@@ -0,0 +1,44 @@
+export const tableData = [
+    {
+      name: "Miles, Esther",
+      category: "individual",
+      user_alias: ["whiteswan331", "bluebear234"],
+      status: "active",
+    },
+    {
+      name: "Black, Marvin",
+      category: "corporate",
+      user_alias: ["bigbear444", "brownfish258"],
+      status: "liquidated",
+    },
+    {
+      name: "Flores, Juanita",
+      category: "individual",
+      user_alias: ["crazyfish228", "beautifullion765"],
+      status: "active",
+    },
+    {
+      name: "Nguyen, Shane",
+      category: "individual",
+      user_alias: ["tinypanda866", "greenkoala518"],
+      status: "liquidated",
+    },
+    {
+      name: "Henry, Arthur",
+      category: "corporate",
+      user_alias: ["sadwolf227", "beautifulmouse112"],
+      status: "liquidated",
+    },
+    {
+      name: "Cooper, Kristin",
+      category: "corporate",
+      user_alias: ["ticklishkoala607", "purpleleopard757"],
+      status: "liquidated",
+    },
+    {
+      name: "Cooper, Kristin",
+      category: "corporate",
+      user_alias: ["lazymeercat616", "whiterabbit554"],
+      status: "liquidated",
+    },
+  ];
